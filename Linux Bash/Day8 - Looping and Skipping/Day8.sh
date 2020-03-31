9cdcf4c0331ba67c72ab1b7edc227ba96d6d909f
Ankit Yadav <ankitjnyadav@gmail.com>
2020-03-31 21:30:56 +0530

    Day8 - Looping and Skipping

---
+++ Linux Bash/Day8 - Looping and Skipping/Day8.sh
@@ -0,0 +1,8 @@
+#Problem Statement - https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem
+for i in {1..99}
+do
+  if [ $(($i%2)) -ne 0 ]
+  then
+    echo $i
+  fi
+done
