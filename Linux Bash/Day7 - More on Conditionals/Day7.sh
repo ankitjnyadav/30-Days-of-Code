5b86ea83e694f5b38c9093c3444f55684df76655
Ankit Yadav <ankitjnyadav@gmail.com>
2020-03-31 21:30:35 +0530

    Day7 - More on Conditionals

---
+++ Linux Bash/Day7 - More on Conditionals/Day7.sh
@@ -0,0 +1,14 @@
+#Problem Statement - https://www.hackerrank.com/challenges/bash-tutorials---more-on-conditionals/problem
+read a
+read b
+read c
+
+if [ $a == $b ] && [ $b == $c ]
+then
+  echo 'EQUILATERAL'
+elif [ $a != $b ] && [ $b != $c ] && [ $a != $c ]
+then
+  echo 'SCALENE'
+else
+  echo 'ISOSCELES'
+fi
