153653f0e70576e1cd3beefa5ff0b1f90105d6f7
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-13 23:15:10 +0530

    Day 28: RegEx, Patterns, and Intro to Databases

--- 30 Days of Code/Day28 - RegEx, Patterns, and Intro to Databases/Day28.py
+++ 30 Days of Code/Day28 - RegEx, Patterns, and Intro to Databases/Day28.py
@@ -1,3 +1,21 @@
 '''
 Problem Statement - https://www.hackerrank.com/challenges/30-regex-patterns/problem
 '''
+
+import re
+
+if __name__ == '__main__':
+    N = int(input())
+    pattern = '.+@gmail\.com'
+    name = []
+    for N_itr in range(N):
+        first_name_email_id = input().split()
+        firstName = first_name_email_id[0]
+        emailID = first_name_email_id[1]
+        result1 = re.match(pattern, emailID)
+        if result1:
+            name.append(firstName)
+
+    name.sort(reverse=False)
+    for ele in name:
+        print(ele)
