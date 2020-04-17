921bb8d7b0a65fe3be608e6ebd55b7a29aa0fe49
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-17 23:19:48 +0530

    Day 2: More Dice

--- 10 Days of Statistics/Day2: More Dice/Day2_1.py
+++ 10 Days of Statistics/Day2: More Dice/Day2_1.py
@@ -1,3 +1,11 @@
 '''
-Problem Statement -
+Problem Statement - https://www.hackerrank.com/challenges/s10-mcq-2/problem
+'''
+
+'''
+Objective
+In this challenge, we practice calculating probability. We recommend you review the previous challenge's Tutorial before attempting this problem.
+
+Task
+In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of 6. 
 '''
