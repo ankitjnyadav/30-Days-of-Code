fe5d39406f1c2dca254e98443d6b8118770321e3
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-17 23:19:17 +0530

    Day 2: Basic Probability

--- 10 Days of Statistics/Day2: Basic Probability/Day2.py
+++ 10 Days of Statistics/Day2: Basic Probability/Day2.py
@@ -7,8 +7,7 @@ Objective
 In this challenge, we practice calculating probability. Check out the Tutorial tab for a breakdown of probability fundamentals!
 
 Task
-In a single toss of
-fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most
+In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most 9.
 '''
 
 #Ans: 5 / 6
