0768ad902167e73a9766a17f287a877e7117c1d0
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-17 23:26:02 +0530

    Day 2: Compound Event Probability

---
+++ 10 Days of Statistics/Day2: Compound Event Probability/Day2_2.py
@@ -0,0 +1,18 @@
+'''
+Problem Statement - https://www.hackerrank.com/challenges/s10-mcq-3/problem
+'''
+
+'''
+Objective
+In this challenge, we practice calculating the probability of a compound event. We recommend you review today's Probability Tutorial before attempting this challenge. 
+
+
+Task
+There are urns labeled X, Y, and Z
+Urn X contains 4 red balls and 3black balls. 
+Urn Y contains 5 red balls and 4black balls. 
+Urn Z contains 4 red balls and 4black balls. 
+
+One ball is drawn from each of the 3urns. What is the probability that, of the 3balls drawn,2 are red and 1 is black? 
+'''
+#Answer - 17/42
