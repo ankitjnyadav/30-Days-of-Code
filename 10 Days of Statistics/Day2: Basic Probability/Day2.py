1f151f01470b4e676fda309df5292fe2623f9ee3
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-17 23:16:27 +0530

    Day 2: Basic Probability

---
+++ 10 Days of Statistics/Day2: Basic Probability/Day2.py
@@ -0,0 +1,14 @@
+'''
+Problem Statement - https://www.hackerrank.com/challenges/s10-mcq-1/problem
+'''
+
+'''
+Objective
+In this challenge, we practice calculating probability. Check out the Tutorial tab for a breakdown of probability fundamentals!
+
+Task
+In a single toss of
+fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most
+'''
+
+#Ans: 5 / 6
