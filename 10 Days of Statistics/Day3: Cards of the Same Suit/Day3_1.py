998702d9d5b321da2e4f772de6db392bf56fa2cc
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-18 23:14:30 +0530

    Day 3: Cards of the Same Suit

---
+++ 10 Days of Statistics/Day3: Cards of the Same Suit/Day3_1.py
@@ -0,0 +1,8 @@
+'''
+Problem Statement - https://www.hackerrank.com/challenges/s10-mcq-5/problem
+'''
+
+'''
+You draw 2cards from a standard 52-card deck without replacing them. What is the probability that both cards are of the same suit?
+'''
+#Ans:12/51
