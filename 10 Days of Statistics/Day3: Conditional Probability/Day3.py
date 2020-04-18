8f329daa0831f67496d9e558e18a6cbd32fbc796
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-18 23:09:53 +0530

    Day 3: Conditional Probability

---
+++ 10 Days of Statistics/Day3: Conditional Probability/Day3.py
@@ -0,0 +1,7 @@
+'''
+Problem Statement - https://www.hackerrank.com/challenges/s10-mcq-4/problem
+'''
+'''
+Task 2 Suppose a family has children, one of which is a boy. What is the probability that both children are boys?
+'''
+#Ans: 1/3
