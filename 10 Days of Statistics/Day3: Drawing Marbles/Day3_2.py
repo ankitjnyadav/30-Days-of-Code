f40914207f1b6e6945894fe1f84c26b161eb20dd
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-18 23:16:49 +0530

    Day 3: Drawing Marbles

---
+++ 10 Days of Statistics/Day3: Drawing Marbles/Day3_2.py
@@ -0,0 +1,7 @@
+'''
+Problem Statement - https://www.hackerrank.com/challenges/s10-mcq-6/problem
+'''
+'''
+A bag contains 3red marbles and 4blue marbles. Then, 2marbles are drawn from the bag, at random, without replacement. If the first marble drawn is red, what is the probability that the second marble is blue?
+'''
+#Ans:2/3
