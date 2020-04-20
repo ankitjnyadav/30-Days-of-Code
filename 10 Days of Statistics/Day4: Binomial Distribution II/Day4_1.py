767abccb2e0b80c275f2d5af5cb9f64826035076
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-20 23:41:47 +0530

    Day 4: Binomial Distribution II

--- 10 Days of Statistics/Day4: Binomial Distribution II/Day4_1.py
+++ 10 Days of Statistics/Day4: Binomial Distribution II/Day4_1.py
@@ -1,19 +1,12 @@
 '''
 Problem Statement - https://www.hackerrank.com/challenges/s10-binomial-distribution-2/problem
 '''
-'''
-n = number of trials = 10
-p = probability of reject in each trial = 0.12
-x = number of reject
-Probability of binomial distribution = nCx * (p**x) * ((1-p)**(n-x))
-where nCx = n! / x! * (n-x)!
-'''
 
 from math import factorial
 
-def bionomial(x,n,p):
+def bionomial_distro(x, n, p):
     factional_part = factorial(n)/(factorial(x)*factorial(n-x))
-    result =factional_part*p**x*(1-p)**(n-x) #q =(n-x)
+    result =factional_part*p**x*(1-p)**(n-x)
     return result
 
 
@@ -22,12 +15,10 @@ if __name__ == "__main__":
     p = rej_per/100
     ans1 = 0
     ans2=0
-    #No more than 2 rejects(0,1,2)
     for x in range(3):
-        ans1+= bionomial(x,n,p)
+        ans1+= bionomial_distro(x, n, p)
     print(round(ans1,3))
 
-    #At least 2 rejects(2,3,4,....,10)
     for x in range(2,n+1):
-        ans2+= bionomial(x,n,p)
+        ans2+= bionomial_distro(x, n, p)
     print(round(ans2,3))
