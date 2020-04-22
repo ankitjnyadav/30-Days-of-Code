06b5a6c566fe873480dd0e8416e40b4e6ab7ca36
Ankit Yadav <ankitjnyadav@gmail.com>
2020-04-22 21:22:53 +0530

    Day 5: Poisson Distribution I

---
+++ 10 Days of Statistics/Day5: Poisson Distribution I/Day5.py
@@ -0,0 +1,34 @@
+'''
+Problem Statement - https://www.hackerrank.com/challenges/s10-poisson-distribution-1/problem
+'''
+
+'''
+
+    e: A constant equal to approximately 2.71828. (Actually, e is the base of the natural logarithm system.)
+    μ: The mean number of successes that occur in a specified region.
+    x: The actual number of successes that occur in a specified region.
+    P(x; μ): The Poisson probability that exactly x successes occur in a Poisson experiment, when the mean number of successes is μ.
+
+    Poisson Formula. Suppose we conduct a Poisson experiment, in which the average number of successes within a given region is μ. Then, the Poisson probability is:
+    P(x; μ) = (e^ -μ) (μ^ x) / x!
+    where x is the actual number of successes that result from the experiment, and e is approximately equal to 2.71828.
+
+'''
+
+from math import factorial
+from math import e
+
+
+def cal_poisson_distribution(mean,actual_no):
+    return (e**-mean)*(mean**actual_no)/factorial(actual_no)
+
+def decimal_print(value):
+    formatted_float = "{:.3f}".format(value)
+    print(formatted_float)
+
+if __name__ == "__main__":
+    mean = float(input())
+    actual_no = float(input())
+    decimal_print(cal_poisson_distribution(mean,actual_no))
+
+
